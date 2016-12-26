49a990b91867ccf13e66d9a90db521742e8e6be2
edisonfm <edisonferraz@gmail.com>
2016-12-26 15:49:06 -0200

    Simplificando requisições Ajax com a Fetch API

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,5 +1,36 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText)
+        return res;
+    }
+
+    get(url) {
+
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+    }
+
+    post(url, dado) {
+
+        return fetch(url, {
+            headers : { 'Content-type' : 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
+
+    }
+
+}
+
+
+
+
+/*
+class HttpService {
+
   get(url) {
 
     return new Promise((resolve, reject) => {
@@ -55,4 +86,4 @@ class HttpService {
 
   }
 
-}
+}*/
